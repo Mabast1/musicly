2f946181ec8e586ec2b3ddb9bfa3b29f3f6a44a3
Mabast <mabast.niga@yahoo.com>
2022-09-28 11:52:47 -0500

    created api calls to shazamcore rapidApi lib

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "42714c2428mshbf48b453661728ep168231jsn2d4e283d5591"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
