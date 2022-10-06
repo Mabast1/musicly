76a34455b87fd207d3c554a300636abcbc365ae0
Mabast <mabast.niga@yahoo.com>
2022-10-06 01:21:24 -0500

    created top charts component

---
+++ src/components/TopChartCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import PlayPause from "./PlayPause";
+
+const TopChartCard = ({
+  song,
+  i,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+}) => {
+  return (
+    
+  );
+};
+
+export default TopChartCard;
