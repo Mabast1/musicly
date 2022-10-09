925b7913660a7262061606c54024c4e318771e05
Mabast <mabast.niga@yahoo.com>
2022-10-09 02:10:31 -0500

    changed styling

--- src/components/Navlinks.jsx
+++ src/components/Navlinks.jsx
@@ -5,12 +5,12 @@ import { links } from "../assets/constants";
 
 const Navlinks = ({ handleClick }) => {
   return (
-    <div className="mt-10">
+    <div className="mt-5">
       {links.map((link) => (
         <NavLink
           key={link.name}
           to={link.to}
-          className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-white"
+          className="flex flex-row justify-start items-center my-5 text-sm font-medium text-gray-400 hover:text-white"
           onClick={() => handleClick && handleClick()}
         >
           <link.icon className="w-6 h-6 mr-2" />
