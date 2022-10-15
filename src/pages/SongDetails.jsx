4fcd5486c61c650a741fc1027c606db91aae5dda
Mabast <mabast.niga@yahoo.com>
2022-10-15 00:13:27 -0500

    created song details component and rendered lyrics for the current song

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -3,21 +3,33 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
 import { setActiveSong, playPause } from "../redux/features/playerSlice";
+import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
 
 const SongDetails = () => {
   const { songid } = useParams();
   const dispatch = useDispatch();
-  const { setActiveSong, isPlaying } = useSelector((state) => state.player);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  const { data: songData, isFetching: isFetchingSongDetails } =
+    useGetSongDetailsQuery({ songid });
 
   console.log(songid);
 
   return (
     <div className="flex flex-col">
-      {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
+      <DetailsHeader songData={songData} />
 
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics</h2>
-        <div className="mt-5">{}</div>
+        <div className="mt-5">
+          {songData?.sections[1].type === "LYRICS" ? (
+            songData?.sections[1].text.map((line, idx) => (
+              <p className="text-gray-300 text-base my-1">{line}</p>
+            ))
+          ) : (
+            <p>Sorry, no lyrics found</p>
+          )}
+        </div>
       </div>
     </div>
   );
