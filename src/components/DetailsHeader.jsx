3e2636f01042c7033f9d254ae71de4159b164c95
Mabast <mabast.niga@yahoo.com>
2022-10-15 00:14:16 -0500

    added songdetail artist image & name and song name

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,5 +1,42 @@
-const DetailsHeader = () => (
-  <div>DetailsHeader</div>
-);
+import React from "react";
+import { Link } from "react-router-dom";
+
+const DetailsHeader = ({ songData }) => {
+  const artistId = false;
+  return (
+    <div className="relative w-full flex flex-col">
+      <div className="w-full bg-gradient-to-1 from-transparent to-black sm:h-48 h-28" />
+
+      <div className="absolute inset-0 flex items-center">
+        <img
+          src={
+            artistId
+              ? artistData?.artists[artistId].attributes?.artwork?.url
+                  .replace("{w}", "500")
+                  .replace("{h}", "500")
+              : songData?.images?.coverart
+          }
+          alt="profile"
+          className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black"
+        />
+
+        <div className="ml-5">
+          <p className="font-bold sm:text-3xl text-xl text-white">
+            {artistId
+              ? artistData?.artists[artistId].attributes?.name
+              : songData?.title}
+          </p>
+          {!artistId && (
+            <Link to={`/artists/${songData?.artists[0]?.adamid}`}>
+              <p className="text-base text-gray-400 mt-2">
+                {songData?.subtitle}
+              </p>
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default DetailsHeader;
